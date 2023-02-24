8cba54680be859d77a43c160df2f3577db74947a
Younes Makhchan <younessmakhchane@gmail.com>
2023-02-24 17:10:42 +0100

    back

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 const main=async ()=>{
     try{
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster.nszsxvk.mongodb.net/smartRabbit?retryWrites=true&w=majority`)
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster.nszsxvk.mongodb.net/SmartRabbitDB?retryWrites=true&w=majority`)
         console.log("database Connected")
         
     }catch(error){
