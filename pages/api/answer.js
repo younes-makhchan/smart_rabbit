45859413ab7327fcac51f7a3ab6014140e3576c0
Younes Makhchan <younessmakhchane@gmail.com>
2023-02-24 17:11:24 +0100

    back

--- pages/api/answer.js
+++ pages/api/answer.js
@@ -1,18 +1,18 @@
 import NextCors from "nextjs-cors";
 import main from "../../database/connection"
 import Answer from "../../database/schema"
-main().catch(error=>console.log(error))
 
 export default async function storeAnswer(req,res){
-   
+    
     await NextCors(req, res, {
-          // Options
-          methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-          origin: '*',
-          optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-       });
-      
-      
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+    
+    main().catch(error=>console.log(error))
+        
     const create=new Answer(req.body);
     create.save().then((r)=>{
             res.status(200).json(create);
