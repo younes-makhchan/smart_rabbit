a201487ca1801788b40befd88e9159c640dd3504
Younes Makhchan <younessmakhchane@gmail.com>
2023-03-30 16:25:21 +0200

    update alt img

--- components/installPWA/InstallPWA.js
+++ components/installPWA/InstallPWA.js
@@ -18,19 +18,19 @@ const [displayInstallPWA,setDisplayInstallPWA]=useState(true);
       <>
       <div className={style.install_wrapper} style={{display: supportPWA?"":"none"}} onClick={()=>{installPWA.prompt()}}  >
       <button type="button" className={`${style.installbtn} `}  >
-        <img src="google-play-sm.png"  />
+        <img src="google-play-sm.png"  alt="google play store"/>
       </button>
       </div>
       <div className={style["content-wrapper"]} style={{display:(displayInstallPWA&&supportPWA)?"block":"none"}}>
         <div className={style["overlay"]}></div>
         <div className={style["content"]}>
-          <img src="icon-192x192.png"  className={style.icon}/>
+          <img src="icon-192x192.png"  className={style.icon} alt="smart rabbit download"/>
           <h3 className={style["description"]}>
             <span style={{color:"#76c836"}}>Download</span> Smart Rabbit 
           </h3>
           <div className={style.btn__wrapper} >
-          <button type="button"   className={style.install} onClick={()=>installPWA.prompt()}><img src="app-store.png" /></button>
-          <button type="button"  className={style.install} onClick={()=>installPWA.prompt()}><img src="play-store.png" /></button>
+          <button type="button"   className={style.install} onClick={()=>installPWA.prompt()}><img src="app-store.png" alt="app store"/></button>
+          <button type="button"  className={style.install} onClick={()=>installPWA.prompt()}><img src="play-store.png" alt="play store" /></button>
           </div>
           <div>
           <button type="button" className={style["not_now"]} onClick={()=>setDisplayInstallPWA(false)}>Not Now</button>
