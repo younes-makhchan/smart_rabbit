8aac76bf2d79f513483f3013b9f51b4de67950bb
Younes Makhchan <younessmakhchane@gmail.com>
2023-04-02 20:01:24 +0200

    lunch bing clarity,google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,14 @@
-// import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-// import * as gtag from '../lib/gtag'
+
 import { DefaultSeo } from 'next-seo'
 import SEO from "../next-seo.config"
 import "./style.css"
-import Script from 'next/script'
 const App = ({ Component, pageProps }) => {
-  // const router = useRouter()
-  // useEffect(() => {
-  //   const handleRouteChange = (url) => {
-  //     gtag.pageview(url)
-  //   }
-  //   router.events.on('routeChangeComplete', handleRouteChange)
-  //   return () => {
-  //     router.events.off('routeChangeComplete', handleRouteChange)
-  //   }
-  // }, [router.events])
+  
 
   return(
       <>
         
-     <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-VLEB4TRHNW" ></Script>
-      <Script strategy='afterInteractive' id="google-analytics">
-     {` window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-VLEB4TRHNW');`}
-    </Script>
+    
       <DefaultSeo {...SEO} />
 
      <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-// import { GA_TRACKING_ID } from '../lib/gtag'
-
+import Script from 'next/script'
 export default class MyDocument extends Document {
 
 
@@ -9,29 +8,32 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+        <Script  strategy='afterInteractive'>
+      {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      console.log(t)
+    })(window, document, "clarity", "script", "ghdubkv8u6");
+    `}
+    </Script>
           {/* <!-- Clarity tracking code for https://smart-rabbit.netlify.app/ --> */}
-        
           <link rel="manifest" href="/manifest.json"></link>
          
-        {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9485022839769098"
-     crossOrigin="anonymous"></script> */}
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {/* <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          /> */}
+        {/* <Script strategy="afterInteractive" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9485022839769098"
+     crossOrigin="anonymous"></Script> */}
+         
+    <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-VLEB4TRHNW" ></Script>
+      <Script strategy='afterInteractive' id="google-analytics">
+     {` window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-VLEB4TRHNW');`}
+    </Script>
+
+    
         </Head>
         <body>
           <Main />
