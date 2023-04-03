00e9bca158a0d30f4b087f2399a336613184e349
Younes Makhchan <younessmakhchane@gmail.com>
2023-04-03 15:32:33 +0200

    update h1 to h2

--- components/About/About.js
+++ components/About/About.js
@@ -8,7 +8,7 @@ const About=()=>{
         <section className={style.section} id="about">
                 <article className={style.article} >
                 <div className={style.content}>
-                <h1 className={style.title}>About Smart Rabbit</h1>
+                <h2 className={style.title}>About Smart Rabbit</h2>
                 <p className={style.description}>
                 Smart Rabbit is the ultimate solution for parents seeking a fun and engaging way to educate and entertain their kids. This innovative AI-powered tool understands children's questions, providing accurate and age-appropriate answers that help them learn and grow. With features such as speech recognition and image-to-text translation, Smart Rabbit covers all children's needs, ensuring they have access to the resources and advice they need to succeed. Available in multiple languages, Smart Rabbit is the go-to tool for parents worldwide looking to provide their children with a rich and interactive learning experience.
 
